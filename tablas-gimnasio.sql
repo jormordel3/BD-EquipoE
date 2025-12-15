e5bba0852025d72ebd33a50a19b2ea376401af08
akurri <141771971+akurri@users.noreply.github.com>
2025-12-15 12:24:25 +0100

    Refactor SQL schema for gym database

--- tablas-gimnasio.sql
+++ tablas-gimnasio.sql
@@ -2,27 +2,32 @@ create database GIMNASIO;
 
 use GIMNASIO;
 
+create table MEMBRESIA(
+  idMembresia int AUTO_INCREMENT,
+  precio decimal(5,2) not null,
+  tipo varchar(64) not null,
+  constraint PK_MEMBRESIA primary key(idMembresia));
+
+
 create table CLIENTE(
-  idCliente int,
+  idCliente int AUTO_INCREMENT,
   nombre varchar(128) not null,
   apellidos varchar(320) not null,
   edad int,
-  direccion varchar(128),
-  telefono varchar(11) not null,
   email varchar(128) not null,
+  calle VARCHAR(100),
+  numero VARCHAR(10),
+  ciudad VARCHAR(50),
+  cp VARCHAR(10),
+  telefono varchar(15) not null,
   fechaRegistro date,
-  constraint PK_CLIENTE primary key(idCliente));
+  membresia int,
+  constraint PK_CLIENTE primary key(idCliente),
+  constraint FK_CLIENTE_MEMBRESIA foreign key(membresia) references MEMBRESIA(idMembresia)); 
 
-create table MEMBRESIA(
-  idMembresia int,
-  precio decimal(5,2) not null,
-  tipo varchar(64) not null,
-  cliente int,
-  constraint PK_MEMBRESIA primary key(idMembresia),
-  constraint FK_MEMBRESIA foreign key(cliente) references CLIENTE(idCliente));
 
 create table MONITOR(
-  idMonitor int,
+  idMonitor int AUTO_INCREMENT,
   nombre varchar(128) not null,
   apellidos varchar(128) not null,
   dni varchar(11) not null,
@@ -34,28 +39,29 @@ create table MONITOR(
   constraint CHK_SALARIO check(salario>=1000));
 
 create table GRUPO_MUSCULAR(
-  idMusculo int,
+  idMusculo int AUTO_INCREMENT,
+  nombre VARCHAR(64),
   descripcion varchar(64),
   imagen blob,
   constraint PK_GRPMSC primary key(idMusculo));
 
 create table EJERCICIO(
+  idEjercicio int AUTO_INCREMENT,
   nombre varchar(64),
-  peso decimal(3,2),
+  peso decimal(6,2),
   series int,
   numRepeticiones int,
-  duracion decimal(2,2),
+  duracion decimal(10,2),
   musculo int,
-  constraint PK_EJERCICIO primary key(nombre),
-  constraint FK_EJERCICIO foreign key(musculo) references GRUPO_MUSCULAR(idMusculo));
+  constraint PK_EJERCICIO primary key(idEjercicio),
+  constraint FK_EJERCICIO_MUSCULO foreign key(musculo) references GRUPO_MUSCULAR(idMusculo));
 
 create table CLASE(
+  idClase int AUTO_INCREMENT,
   tipo  varchar(32),
   horario datetime not null,
-  duracion decimal(2,2) not null,
+  duracion int not null,
   aforoMax int not null,
-  nombreEjercicio varchar(64),
   monitor int,
-  constraint PK_CLASE primary key(tipo),
-  constraint FK_CLASE foreign key(nombreEjercicio) references EJERCICIO(nombre),
-  constraint FK_CLASE2 foreign key(monitor) references MONITOR(idMonitor));
+  constraint PK_CLASE primary key(idClase),
+  constraint FK_CLASE_MONITOR foreign key(monitor) references MONITOR(idMonitor));
